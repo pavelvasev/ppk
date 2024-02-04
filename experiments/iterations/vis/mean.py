f4f5a55b3fba08ae9bad4a1fb3720976dae9fddb
Pavel Vasev <pavel.vasev@gmail.com>
2024-02-05 01:40:08 +0500

    better

---
+++ experiments/iterations/vis/mean.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+print(df)
+
+#df2 = df["SECONDS"].mean()
+
+#df = df[ df["DN"] == 100000 ].drop( ["DN"],axis=1 )
+df2 = df.groupby( ["CMD","DN","P"] ).mean()
+#df2 = df.groupby( ["CMD","P"] ).mean() #.groupby( ["P"] )
+print(df2)
+df2.to_csv('mean.csv')
+
+exit()
+
