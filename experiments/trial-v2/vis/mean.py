0f5180e13c98545779dbc3c52b93310a96859218
Pavel Vasev <pavel.vasev@gmail.com>
2024-02-06 16:08:03 +0500

    min

--- experiments/trial-v2/vis/mean.py
+++ experiments/trial-v2/vis/mean.py
@@ -8,7 +8,7 @@ print(df)
 #df2 = df["SECONDS"].mean()
 
 #df = df[ df["DN"] == 100000 ].drop( ["DN"],axis=1 )
-df2 = df.groupby( ["CMD","DN","P"] ).mean()
+df2 = df.groupby( ["CMD","DN","P"] ).min()
 #df2 = df.groupby( ["CMD","P"] ).mean() #.groupby( ["P"] )
 print(df2)
 df2.to_csv('mean.csv')
