6f06fa9b9aee4ff4b6f3cb734859fa6e6540dd6f
Pavel Vasev <pavel.vasev@gmail.com>
2024-02-10 23:50:18 +0500

    gr5

---
+++ client-api/js/gr5.js
@@ -0,0 +1,25 @@
+// скрипт запуска GR5 из ППК программ
+// сделан внешним по отношению к папке gr5 т.к. там свой package.json
+
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import * as cp from 'node:child_process'
+import * as fs from 'node:fs'; // https://nodejs.org/api/fs.html
+
+/*
+  идеи
+  - как-то бы не печатать логи но похоже --silent отрубает и инфо о порту.
+
+  сделано
+  - ключ для авто-открытия браузера. почему бы и нет. 
+*/
+
+export function start( open_browser=false ) {  
+    let cwd = path.resolve( __dirname, "gr5/dist" )
+    let args = [`http-server`] //,"--silent"] 
+    // --silent = не печатать логи 
+    if (open_browser) args.push("-o")
+  	let prg = cp.spawn( `npx`, args,{cwd,detached:true,stdio: ['ignore','inherit','ignore']} )
+  	return prg
+}
