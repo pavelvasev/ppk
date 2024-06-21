447b05cdccfed3c470868a728c050c6972249781
Pavel Vasev <pavel.vasev@gmail.com>
2024-06-21 22:13:13 +0500

    local-channels-cpp

--- client-api/cpp-local-channels/channels.h
+++ client-api/cpp-local-channels/channels.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <vector>
+#include <map>
 
 /* делаем каналы. локальные.
 
@@ -12,23 +13,36 @@
 
 // канал
 template<typename T>
-class Channel {
+class channel {
 
 	typedef std::function<void (T)> reaction_fn;
 
 public:	
-	Channel() {}
-	std::vector<reaction_fn> subscribers;
+	channel() { 	  
+   }
+	std::map<int,reaction_fn> subscribers;
+	int next_subscriber_id = 0;
 
     // todo в будущем тут не воид а промиса что данные ушли
 	void submit( T value ) {
-		for (const reaction_fn &f : subscribers) {
-			f( value );
+
+		for (auto i = subscribers.begin(); i != subscribers.end(); i++) {
+			i->second( value );
 		}
+		/*
+		for (const auto & [id,f] : subscribers) {
+			f( value );
+		}*/
 	}
 
-	auto subscribe( reaction_fn fn) {
-		subscribers.push_back( fn );
+	auto subscribe( reaction_fn fn) {		
+		int this_subscriber_id = next_subscriber_id++;
+		subscribers[this_subscriber_id] = fn;
+
+		auto unsub = [this,this_subscriber_id]() {
+			subscribers.erase( this_subscriber_id );
+		};
+		return unsub;
 
 		/*
 		https://stackoverflow.com/questions/20833453/comparing-stdfunctions-for-equality
@@ -36,7 +50,7 @@ public:
 
 		думал по находить значению.. но нет. ну стало быть - будем по ключу..
 		стало быть мэпа нужна
-		*/
+		
 
 		auto unsub = [this,fn]() {
 			std::vector<reaction_fn> new_subscribers;
@@ -47,18 +61,26 @@ public:
 			subscribers = new_subscribers;
 		};
 		return unsub;
+		*/
 	}
 };
 
 // связь
 template<typename T>
-class Link {
-
-	typedef std::function<void (T)> reaction_fn;
+class link {
 
 public:	
-	Link() {}
-	~Link() {
+	link( ) {}
+	
+	link(channel<T> &a, channel<T> &b ) {
+		init(a,b);
+	}
+
+	~link() {
+		stop();
+	}
+
+	void stop() {
 		if (unsub) {
 			unsub();
 			unsub = 0;
@@ -67,12 +89,46 @@ public:
 
 	std::function<void()> unsub = 0;
 
-	Link& init( Channel<T> a, Channel<T> b ) {
-		auto f = [&b](T&val) {
+	void init( channel<T> &a, channel<T> &b ) {
+		stop();
+		auto f = [&b](T val) {
 			b.submit( val );
 		};
 		unsub = a.subscribe( f );
-	}	
+	};
+};
+
+// реакция
+template<typename T>
+class react {
+
+	typedef std::function<void (T)> reaction_fn;
+
+public:	
+	react( ) {}
+	
+	react(channel<T> &a, reaction_fn f ) {
+		//printf("RRR\n");
+		init(a,f);
+	}
+
+	~react() {
+		stop();
+	}
+
+	void stop() {
+		if (unsub) {
+			unsub();
+			unsub = 0;
+		}
+	}
+
+	std::function<void()> unsub = 0;
+
+	void init( channel<T> &a, reaction_fn f ) {
+		stop();
+		unsub = a.subscribe( f );
+	};
 };
 
 #endif
--- client-api/cpp-local-channels/test-channels.cpp
+++ client-api/cpp-local-channels/test-channels.cpp
@@ -2,16 +2,83 @@
 
 #include "channels.h"
 
+class alfa 
+{
+public:
+	channel<int> input;
+	channel<int> output;
+
+	alfa( int coef ) {
+		/* типа input навсегда засабскрайбился..
+		   если бы оно хотя бы выдавало не функцию отписки,
+		   а объект некий.. хм
+		   но с другой стороны это наш локальный инпут, что хотим то и делаем
+		   */
+
+		input.subscribe( [this,coef](int val) {
+			output.submit( val * coef );
+		});		
+	}
+};
+
+class beta
+{
+public:
+	channel<int> input;
+	channel<int> output;
+
+	react<int> r1; // типа красиво.. и мб технично
+
+	beta( int coef ) {
+		r1.init( input,[this,coef](int val) {
+			output.submit( val * coef );
+		} );
+	}
+};
 
 int main(void) {
 	//printf("hi tid=%x\n",pthread_self());
 
-	Channel<int> a;
-	Channel<int> b;
+	channel<int> a;
+	channel<int> b;
 
 	a.subscribe( [](int val) {
-		printf("see a val %d\n",val);
+		printf("a see a val %d\n",val);
 	});
-		
+
+	a.submit(42);
+
+	// ######################
+	printf("############## II\n");
+
+	b.subscribe( [](int val) {
+		printf("b see a val %d\n",val);
+	});
+
+	link<int> link1(a,b);
+
+	a.submit(43);
+	a.submit(44);
+
+	//////////////////////
+	printf("############## III\n");
+	alfa obj1(10);
+	link<int> link2(a,obj1.input);
+	link<int> link3(obj1.output,b);
+	link1.stop();
+
+	a.submit(55);
+
+	////////////////////////////
+	printf("############## IV\n");
+	{
+		a.submit(56);
+		react<int> r1( b, [](int val) {
+			printf("react r1 see val %d\n",val);
+		});
+		a.submit(57);
+    }
+    a.submit(58);
+
 	printf("finish\n");
 }
