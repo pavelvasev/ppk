733536150b9b141de343319d968bbd345852459d
Pavel Vasev <pavel.vasev@gmail.com>
2024-07-15 22:00:52 +0500

    when-all-py

--- client-api/python/ppk_cells.py
+++ client-api/python/ppk_cells.py
@@ -6,6 +6,9 @@ todo
 - операция отмены чтения
 - объект-связь! и сообразно операция пересылки, по аналогии с do_query_send
 
+- ppk.channel, ppk.when_all .... ? такое было конечно уже с промисами
+  но тут другое..
+
 REASON
 пишем и читаем с ключем value
 читаем тоже с ним иначе глупость получается - записали объект (через submit)
@@ -68,3 +71,36 @@ class Channel:
     async for msg in obj.clicked.read():
         print("clicked! ",msg)
 """         
+
+################################################
+# эксперимент
+# channels есть объекты
+class WhenAll():
+    def __init__( self, rapi, id, *channels ):
+        #self.rapi = rapi
+        #self.id = id
+        self.output = Channel( rapi, id )
+        #self.channels_ids = channels_ids
+        self.channels = channels
+
+    async def init(self):
+        # idea - вот тут видно что это не шибко то subscribe
+        # а нечто общее - ppk.when_any..
+        index = 0
+        self.pending_mask = (2**len(self.channels))-1
+
+        for x in self.channels:
+            #print("when all subscribing to ",x.id,"index=",index)
+            await x.subscribe( lambda z,index=index: self.on_val(index,x,z))
+            index = index+1
+        return self
+
+    async def on_val(self,index,channel,val):
+        self.pending_mask = self.pending_mask & (~(2**index))
+        #print("when-all index",index,"so pending_mask",self.pending_mask,"index=",index,"pow=",2**index,"channel.id=",channel.id,"val",val)
+        if self.pending_mask == 0:
+            await self.do_job()
+
+    async def do_job(self):
+        vals = [x.value for x in self.channels]
+        await self.output.submit( vals )
