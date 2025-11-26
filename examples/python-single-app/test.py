b09929f61b7c7c76d62b933c17e347aa6cf8bb47
Pavel Vasev <pavel.vasev@gmail.com>
2025-11-26 21:08:31 +0500

    single-app-example

---
+++ examples/python-single-app/test.py
@@ -0,0 +1,19 @@
+#!/bin/env python3.9
+
+import asyncio
+import ppk
+
+async def main(rapi):
+
+	def callback(msg):
+		print("callback called! msg=",msg)
+	
+	print("=========== installing query to topic `test2`")
+	await rapi.query( "test2",callback )
+	print("=========== sending message to topic `test2`")
+	await rapi.msg( {"label":"test2","sigma":5} )
+
+	print("============ done")	
+
+ppk.start( main )
+
