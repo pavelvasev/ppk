1c8a11b6b205ee2c06ed59564778e3c3b7a1d1b5
Pavel Vasev <pavel.vasev@gmail.com>
2025-11-27 10:22:03 +0500

    refactor

--- examples/40-subprocesses/app.py
+++ examples/40-subprocesses/app.py
@@ -5,32 +5,35 @@ import ppk
 import os,sys
 import atexit
 
+async def start_worker_process( rapi, cmd_and_args, worker_id ):
+    env = os.environ.copy()
+    env["SERVER_URL"] = rapi.server_url
+    proc = await asyncio.create_subprocess_exec(*cmd_and_args,env=env,stdin=asyncio.subprocess.DEVNULL,start_new_session=True)
+
+    def make_cleanup_fn( proc, worker_id):
+        def cleanup():            
+            if proc.returncode is None:
+                print("sending terminate to worker_id=",worker_id)
+                proc.terminate()
+        return cleanup
+
+    #atexit.register( make_cleanup_fn(proc,worker_id) )
+    # this may be better
+    rapi.atexit( make_cleanup_fn(proc,worker_id) )
+
+
 async def main(rapi):
     print("============== server started. url=",rapi.server_url)
 
     print("starting subprocesses")
     for i in range(2):
-        cmd = sys.executable
         worker_id = f"wrk_{i}"
-        args = ["worker.py",worker_id]
-        env = os.environ.copy()
-        env["SERVER_URL"] = rapi.server_url
-        proc = await asyncio.create_subprocess_exec(cmd,*args,env=env,stdin=asyncio.subprocess.DEVNULL,start_new_session=True)
-
-        def make_cleanup_fn( proc, worker_id):
-            def cleanup():            
-                if proc.returncode is None:
-                    print("sending terminate to worker_id=",worker_id)
-                    proc.terminate()        
-            return cleanup
-
-        #atexit.register( make_cleanup_fn(proc,worker_id) )
-        # this may be better
-        rapi.atexit( make_cleanup_fn(proc,worker_id) )
+        cmd_and_args = [sys.executable, "worker.py",worker_id]
+        await start_worker_process( rapi, cmd_and_args, worker_id )
 
     def callback(msg):
         print("callback called! msg=",msg)
-    
+
     print("=========== installing query to topic `test2`")
     await rapi.query( "test2",callback )
 
--- examples/40-subprocesses/worker.py
+++ examples/40-subprocesses/worker.py
@@ -5,22 +5,22 @@ import ppk
 import os,sys
 
 async def main(rapi):
-	print("=========== worker: connected to server")
-	WORKER_ID=sys.argv[1] # todo change to main arg
-	rapi.sender = WORKER_ID
-	print("WORKER_ID=",WORKER_ID)
+    print("=========== worker: connected to server")
+    WORKER_ID=sys.argv[1] # todo change to main arg
+    rapi.sender = WORKER_ID
+    print("WORKER_ID=",WORKER_ID)
 
-	async def callback(msg):		
-		print("worker: callback called! msg=",msg)
-		print("worker: sending messages to topic `test2`")
-		for i in range(2):
-			sigma = i + msg['start']
-			print("=========== worker: sending message sigma=",sigma)
-			await rapi.msg( {"label":"test2","sigma":sigma} )
-	
-	print("=========== worker: installing query to topic `test1`")
-	await rapi.query( "test1",callback )
+    async def callback(msg):        
+        print("worker: callback called! msg=",msg)
+        print("worker: sending messages to topic `test2`")
+        for i in range(2):
+            sigma = i + msg['start']
+            print("=========== worker: sending message sigma=",sigma)
+            await rapi.msg( {"label":"test2","sigma":sigma} )
+    
+    print("=========== worker: installing query to topic `test1`")
+    await rapi.query( "test1",callback )
 
-	await asyncio.Future()
+    await asyncio.Future()
 
 ppk.start( main,server_url=os.environ["SERVER_URL"] )
